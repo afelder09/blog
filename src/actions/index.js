60a86b727ac628223768daa26eaab011dc8df47f
Anson Felder <afelder09@gmail.com>
2018-09-26 08:44:34 -0400

    Beginning of routing

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import { FETCH_POSTS } from './types';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
+const API_KEY = `?key=FUCKBITCHES1234`
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}posts${API_KEY}`)
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    };
+}
